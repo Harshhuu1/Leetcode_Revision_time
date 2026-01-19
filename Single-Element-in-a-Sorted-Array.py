e69dfd37663b2bd9f5c496f5f876727753e69327
Harsh <ediyalot@gmail.com>
2026-01-19 21:32:57 +0530

    [540] [Time Beats: 100.00%] [Memory Beats: 10.75%] - LeetPush

--- Single-Element-in-a-Sorted-Array.py
+++ Single-Element-in-a-Sorted-Array.py
@@ -1,18 +1,21 @@
-1# from typing import List
-2
-3class Solution:
-4    def singleNonDuplicate(self, nums: List[int]) -> int:
-5        n = len(nums)
-6        high=n-1
-7        low=0
-8        while low<high:
-9            mid=(low+high)//2
-10            if mid%2==1:
-11                mid-=1
-12            if nums[mid]==nums[mid+1]:
-13                low=mid+2
-14            else:
-15                high=mid
-16        return nums[low]
-17        
-18        
+1class Solution:
+2    def singleNonDuplicate(self, nums: List[int]) -> int:
+3        low=0
+4        if len(nums)<=1:
+5            return nums[0]
+6        high=len(nums)-1
+7        while low<high:
+8            mid=(low+high)//2
+9            if mid%2==1:
+10                mid-=1
+11            # print(mid)
+12            # if mid+1<len(nums):
+13            if nums[mid]==nums[mid+1]:
+14                low=mid+2
+15                # print(nums[high])
+16            else:
+17                high=mid
+18            
+19        # if low+1<len(nums):
+20        return nums[low]
+21
