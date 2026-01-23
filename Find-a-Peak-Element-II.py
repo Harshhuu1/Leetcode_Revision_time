3c1f2f61444abde1b3d2c06e02b02303df2412cc
Harsh <ediyalot@gmail.com>
2026-01-23 18:02:36 +0530

    [1901] [Time Beats: 100.00%] [Memory Beats: 26.13%] - LeetPush

--- Find-a-Peak-Element-II.py
+++ Find-a-Peak-Element-II.py
@@ -2,26 +2,25 @@
 2    def findPeakGrid(self, mat: List[List[int]]) -> List[int]:
 3        n=len(mat)
 4        m=len(mat[0])
-5
+5        maxi=-1e9
 6        low=0
-7        high=m-1
-8        maxi=-1e9
-9        index=-1
-10        while low<=high:
-11            mid=(low+high)//2
-12            
-13            for i in range(n):
-14                if mat[i][mid]>maxi:
-15                    maxi=mat[i][mid]
-16                    index=i
-17                left = mat[index][mid - 1] if mid - 1 >= 0 else -1
-18                right = mat[index][mid + 1] if mid + 1 < m else -1
-19            if mat[index][mid]>left and mat[index][mid]>right:
-20                return (index,mid)
-21            elif mat[index][mid]>right:
-22                high=mid
-23            else:
-24                low=mid+1
-25            
-26        return (-1,-1)
-27
+7
+8        high=m-1
+9        while low<=high:
+10            mid=(low+high)//2
+11            for i in range(n):
+12                if maxi<mat[i][mid]:
+13                    maxi=mat[i][mid]
+14                    index=i
+15            left=-1  if mid-1<=0 else mat[index][mid-1]
+16            right=-1 if mid+1>=m else mat[index][mid+1]
+17            
+18            if mat[index][mid]>left and mat[index][mid]>right:
+19                return (index,mid)
+20            elif mat[index][mid]<right:
+21                low=mid+1
+22            else:
+23                high=mid-1
+24        return (-1,-1)
+25
+26
